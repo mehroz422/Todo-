cfd0c06f98ac16bb8eeae7af8f0a78cf68229c56
Mehroz Ahmad <mehroz422@gmail.com>
2022-07-22 04:15:55 -0400

    Initial commit

---
+++ db/migrate/20220721135806_create_todos.rb
@@ -0,0 +1,13 @@
+class CreateTodos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :todos do |t|
+
+      t.text :name
+      t.text :description
+
+    
+
+      t.timestamps
+    end
+  end
+end
